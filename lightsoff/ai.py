46530331d3dc4ccd101bdfd836f62c64188102b1
Nestor Sokil <nestor0603@gmail.com>
2024-11-24 09:33:01 +0100

    Use Anthropic to parse text posts

---
+++ lightsoff/ai.py
@@ -0,0 +1,27 @@
+import os
+import anthropic
+import logging
+import json
+
+prompt = """
+Read message potentially talking about power outage. 
+Return JSON map of ISO day -> boolean if there will be outages. Empty map if nothing related in text.
+"""
+
+def detect(text):
+    if os.environ.get("ANTHROPIC_API_KEY") is None:
+        logging.debug("ANTHROPIC_API_KEY is not set")
+        return {}
+
+    logging.info("Sending message to Anthropics API")
+    client = anthropic.Anthropic()  # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    message = client.messages.create(
+        max_tokens=1000,
+        temperature=0,
+        system=prompt,
+        model="claude-3-5-sonnet-20240620",
+        messages=[{"role": "user", "content": text}])
+    result = message.content[0].text
+    if result is None or result == "":
+        return {}
+    return json.loads(result)
